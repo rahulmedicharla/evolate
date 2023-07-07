31bcba7c53fa796f07f0e96ee6887918cd6941fb
rahulmedicharla <rmedicharla@gmail.com>
2023-07-07 16:46:55 -0400

    working prototype!

--- data_structures/evolate.py
+++ data_structures/evolate.py
@@ -2,7 +2,7 @@
  This is the implementation of a the custom modular datatype Evolate
 
 """
-from data_types import DataType, ResponseType, NodeInterface
+from data_types import DataType, ResponseType, NodeInterface, EvolateNetwork
 from data_structures.node import Node
 from data_structures.singly_linked_list import SinglyLinkedList
 from data_structures.sequence import Sequence
@@ -10,6 +10,9 @@ from data_structures.tree_map import TreeMap
 from data_structures.hash_map import HashMap
 
 import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 
 class Evolate():
@@ -22,11 +25,15 @@ class Evolate():
         -> rep (SubDataStructure): this is the current representation of the data 
 
     """
-    def __init__(self, data_type = DataType) -> None:
+    def __init__(self, data_type = DataType.SINGLY_LINKED_LIST) -> None:
         
         self.rep = None
         self.data_type = None
-        
+
+        self.model = EvolateNetwork()
+        self.model.load_state_dict(torch.load("machine_learning/weights.pt", map_location=torch.device('cpu')))
+        self.model.eval()
+
         """
         realtime features to keep track of
             -> total length: total length of the dataset
@@ -67,6 +74,8 @@ class Evolate():
         self.total_commands += 1
         self.idt_commands += 1
 
+        self.determine_when_to_switch()
+
         temp_node = Node(self.rep.get_length(), value, self.data_type)
         return self.rep.add(temp_node)
     
@@ -75,17 +84,24 @@ class Evolate():
         self.idt_commands += 1
         self.search_list = np.append(self.search_list, key)
 
+        self.determine_when_to_switch()
+
         return self.rep.remove(key)
     
     def get(self, key: int) -> NodeInterface or ResponseType:
         self.total_commands += 1
         self.search_list = np.append(self.search_list, key)
 
+        self.determine_when_to_switch()
+
         return self.rep.get(key)
 
     def update(self, key: int, value: any) -> ResponseType:
         self.total_commands += 1
         self.search_list = np.append(self.search_list, key)
+
+        self.determine_when_to_switch()
+
         return self.rep.update(key, value)
 
     def get_length(self) -> int:
@@ -100,11 +116,15 @@ class Evolate():
     def get_features(self) -> tuple:
         self.insertion_deletion_frequency = round((float(self.idt_commands) / float(self.total_commands)), 3)
 
-        self.search_prediction = np.average(self.search_list)
-        std_dev = np.std(self.search_list)
-        self.search_randomness = round((float(std_dev) / float(self.rep.get_length())) -.25, 3)
+        if len(self.search_list) == 0:
+            self.search_prediction = 0
+            self.search_randomness = 0
+        else:
+            self.search_prediction = np.average(self.search_list)
+            std_dev = np.std(self.search_prediction)
+            self.search_randomness = round((float(std_dev) / float(self.rep.get_length())), 3) or 0
 
-        return self.get_size(), self.insertion_deletion_frequency, self.search_randomness, self.search_prediction
+        return [int(self.get_size()), float(self.insertion_deletion_frequency), float(self.search_randomness), int(self.search_prediction)]
     
     def print_items(self) -> ResponseType:
         return self.rep.iterate(self.print)
@@ -119,8 +139,36 @@ class Evolate():
         print("Type: " + str(node.type))
         return ResponseType.SUCCESS
     
+    def determine_when_to_switch(self):
+        if self.total_commands % 100 == 0 and self.total_commands != 0:
+            self.evaluate_features()
+    
+    def evaluate_features(self) -> ResponseType:
+        features = torch.tensor(self.get_features())
+        with torch.no_grad():
+            prediction = self.model(features)
+            prediction = F.softmax(prediction, dim = 0)
+            
+            data_type = DataType.SINGLY_LINKED_LIST
+            max = prediction[0]
+            if prediction[1] > max:
+                data_type = DataType.SEQUENCE
+                max = prediction[1]
+            if prediction[2] > max:
+                data_type = DataType.TREE_MAP
+                max = prediction[2]
+            if prediction[3] > max:
+                data_type = DataType.HASH_MAP
+                max = prediction[3]
+            
+            if data_type != self.data_type:
+                self.switch_data_structures(data_type)
+                self.data_type = data_type
+
+    
     def switch_data_structures(self, new_type: DataType) -> ResponseType:
         #create new data instance
+        print("Switching Data Structure from " + str(self.data_type) + " to " + str(new_type))
         if new_type == DataType.SEQUENCE:
             new_rep = Sequence()
             self.data_type = new_type
--- main.py
+++ main.py
@@ -1,15 +1,32 @@
-import torch
-from data_types import EvolateNetwork
+from data_structures.evolate import Evolate
 
-model = EvolateNetwork()
-model.load_state_dict(torch.load("machine_learning/weights.pt", map_location=torch.device('cpu')))
+list = Evolate()
 
-model.eval()
+for i in range(0,1000):
+    list.add(i)
 
-with torch.no_grad():
-    data = torch.tensor([0,0.0,0.0,0])
-    out_data = model(data)
-    print(out_data)
-    data = torch.tensor([0,0.2345,0.23456,0])
-    out_data = model(data)
-    print(out_data)
+for i in range(0,100):
+    list.get(i)
+
+
+# from data_types import EvolateNetwork
+# import torch
+# import torch.nn as nn
+# import torch.nn.functional as F
+# import numpy as np
+
+# model = EvolateNetwork()
+# model.load_state_dict(torch.load("machine_learning/weights.pt", map_location=torch.device('cpu')))
+# model.eval()
+
+# with torch.no_grad():
+#     a = 0
+#     b = 0.0
+#     c = 0.0
+#     d = 0
+#     print(type(b))
+#     data = torch.tensor([a,b,c,d])
+#     res = model(data)
+#     output = F.softmax(res, dim=0)
+#     print(output)
+
